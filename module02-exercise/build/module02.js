e0f80eaceb8bf4a36667d76532bacc19cee0644b
Youhua Tang <tangyouhua@gmail.com>
2023-02-01 22:57:33 +0800

    feat: add module02 ex4

--- module02-exercise/build/module02.js
+++ module02-exercise/build/module02.js
@@ -64,3 +64,12 @@ function whichMonths(season) {
     return monthsInSeason;
 }
 console.log(whichMonths(Season.Fall));
+/* EXERCISE 4
+   TODO: Declare the array as the type to match the type of the items in the array. */
+let randomNumbers = [];
+let nextNumber;
+for (let i = 0; i < 10; i++) {
+    nextNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+    randomNumbers.push(nextNumber);
+}
+console.log(randomNumbers);
--- module02-exercise/module02.ts
+++ module02-exercise/module02.ts
@@ -70,3 +70,15 @@ function whichMonths(season: Season) {
 }
 
 console.log(whichMonths(Season.Fall));
+
+/* EXERCISE 4
+   TODO: Declare the array as the type to match the type of the items in the array. */
+
+let randomNumbers: number[] = [];
+let nextNumber;
+for (let i = 0; i < 10; i++) {
+    nextNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+    randomNumbers.push(nextNumber);
+}
+
+console.log(randomNumbers);
