d4f60fc89e6c9e38c5ae6c933acc968d19717cdc
Youhua Tang <tangyouhua@gmail.com>
2023-02-01 22:53:56 +0800

    mod: module02 ex2

--- module02-exercise/build/module02.js
+++ module02-exercise/build/module02.js
@@ -4,20 +4,32 @@
 /*  EXERCISE 1
     TODO: Modify the code to add types to the variable declarations.
     The resulting JavaScript should look the same as the original example when you're done. */
-let firstname;
-let lastname;
+let firstName;
+let lastName;
 let fullName;
 ;
 let age;
 let ukCitizen;
-firstname = 'Rebecca';
-lastname = 'Smith';
+firstName = 'Rebecca';
+lastName = 'Smith';
 age = 42;
 ukCitizen = false;
-fullName = firstname + " " + lastname;
+fullName = firstName + " " + lastName;
 if (ukCitizen) {
     console.log("My name is " + fullName + ", I'm " + age + ", and I'm a citizen of the United Kingdom.");
 }
 else {
     console.log("My name is " + fullName + ", I'm " + age + ", and I'm not a citizen of the United Kingdom.");
 }
+/* EXERCISE 2
+   TODO: You can use types to ensure operation outcomes. Run the code as is and then modify
+   it to have strongly typed variables. Then, address any errors you find so that the result
+   returned to a is 12. */
+let x;
+let y;
+let a;
+;
+x = 5;
+y = 7;
+a = x + y;
+console.log(a);
--- module02-exercise/module02.ts
+++ module02-exercise/module02.ts
@@ -22,3 +22,18 @@ if (ukCitizen) {
 } else {
     console.log("My name is " + fullName + ", I'm " + age + ", and I'm not a citizen of the United Kingdom.");
 }
+
+/* EXERCISE 2
+   TODO: You can use types to ensure operation outcomes. Run the code as is and then modify 
+   it to have strongly typed variables. Then, address any errors you find so that the result 
+   returned to a is 12. */
+
+let x: number;
+let y: number;
+let a: number;;
+
+x = 5;
+y = 7;
+a = x + y;
+
+console.log(a);
